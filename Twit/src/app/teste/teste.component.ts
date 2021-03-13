fbbcc89f977b2dd933583ccc1f613fe7b59dcef2
lucas-thedev <thelucasabud@gmail.com>
2021-03-12 21:36:37 -0300

    examples

---
+++ Twit/src/app/teste/teste.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teste',
+  templateUrl: './teste.component.html',
+  styleUrls: ['./teste.component.css']
+})
+export class TesteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  sendAlert() {
+    window.alert('oi')
+  }
+
+}
