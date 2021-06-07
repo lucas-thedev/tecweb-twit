82bbca187911754a9533d0826da305c18c2ad6b3
jaao-web <joaovgarciaa@gmail.com>
2021-06-06 22:04:47 -0300

    jao

--- Twit/src/app/post/post.component.ts
+++ Twit/src/app/post/post.component.ts
@@ -14,15 +14,33 @@ import { DatePipe } from '@angular/common';
 
 export class PostComponent implements OnInit {
 
-
-  @Input() posts: IPost[] = [];
+  liked = 0;
+  countlikes = 0;
+  @Input() posts: IPost[]= [];
   @Input() updatePosts: Function = () => console.log('updated');
   storage = []
 
   constructor(private http: HttpClient, private router: Router, private toastr: ToastrService, private datePipe: DatePipe) { 
   }
 
-  ngOnInit(): void {  
+  ngOnInit(): void {
+    setTimeout(() => {
+      const idPost = this.posts[0].id_twiit 
+    const idUser = sessionStorage.getItem('user') ? sessionStorage.getItem('user') : 0  
+    this.http.get(`http://localhost:3000/liked/${idUser}/${idPost}`).subscribe((response: any) => {
+      this.liked = response[0].liked
+    }, error => {
+      console.log(error)
+      this.toastr.error(error.error)
+    });
+    this.http.get(`http://localhost:3000/like/count/${idPost}`).subscribe((response: any) => {
+        this.countlikes= response[0].count
+
+    }, error => {
+      console.log(error)
+      this.toastr.error(error.error)
+    });
+    }, 1000 );
   }
 
   retwitt(post: IPost): void {
@@ -55,11 +73,26 @@ export class PostComponent implements OnInit {
 
 
     this.http.post(`http://localhost:3000/like/${idUser}/${idPost}`, {}).subscribe((res: any) => {
-      this.toastr.success('Curtida realizada.', 'Sucesso!')
+      const data = res[0]
+      this.http.get(`http://localhost:3000/liked/${idUser}/${idPost}`).subscribe((response: any) => {
+      this.liked = response[0].liked
     }, error => {
       console.log(error)
       this.toastr.error(error.error)
     });
+      this.http.get(`http://localhost:3000/like/count/${idPost}`).subscribe((response: any) => {
+        this.countlikes= response[0].count
+
+    }, error => {
+      console.log(error)
+      this.toastr.error(error.error)
+    });
+    }, error => {
+      console.log(error)
+      this.toastr.error(error.error)
+    });
+
+    
 
     if (this.updatePosts) {
       this.updatePosts()
